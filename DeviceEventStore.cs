67e0aaba7d38b1a94b9a4704a105e073634990ed
Morayya Jain <morayyajain@gmail.com>
2026-09-13 11:15:35 +1000

    deviceEventStore exercise

--- DeviceEventStore.cs
+++ DeviceEventStore.cs
@@ -20,24 +20,39 @@ public class DeviceEvent
 
 public class DeviceEventStore
 {
-    private readonly List<DeviceEvent> _events = new();
-    private readonly HashSet<string> _eventIds = new();
+    private readonly List<DeviceEvent> _events = [];
+    private readonly HashSet<string> _eventIds = [];
+    private readonly Dictionary<string, int> _eventTypes = [];
+    private readonly Dictionary<string, List<DeviceEvent>> _deviceEvents = [];
 
     public bool AddEvent(DeviceEvent deviceEvent)
     {
-        // TODO
-        throw new NotImplementedException();
+        // Checking for duplicate event IDs
+        if (_eventIds.Contains(deviceEvent.Id)) return false;
+        
+        _events.Add(deviceEvent);
+        _eventIds.Add(deviceEvent.Id);
+        
+        // Used for summing events by type
+        if (!_eventTypes.ContainsKey(deviceEvent.Type)) _eventTypes[deviceEvent.Type] = 0;
+        _eventTypes[deviceEvent.Type]++;
+
+        // Used to allocate events to devices
+        if (!_deviceEvents.ContainsKey(deviceEvent.DeviceId)) _deviceEvents[deviceEvent.DeviceId] = [];
+        _deviceEvents[deviceEvent.DeviceId].Add(deviceEvent);
+        
+        return true;
     }
 
     public List<DeviceEvent> GetEventsForDevice(string deviceId)
     {
-        // TODO
-        throw new NotImplementedException();
+        if (!_deviceEvents.ContainsKey(deviceId)) return [];
+        return _deviceEvents[deviceId].OrderBy(deviceEvent => deviceEvent.Timestamp).ToList();
     }
 
     public int CountEventsByType(string type)
     {
-        // TODO
-        throw new NotImplementedException();
+        if (!_eventTypes.ContainsKey(type)) return 0;
+        return _eventTypes[type];
     }
 }
